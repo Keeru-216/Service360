432a0efbb75324ef2e82e89de5ac776ec747145f
Keerthi <keerthirjs7@gmail.com>
2024-09-22 11:53:31 +0530

    1stCommitScreen

---
+++ src/Components/AdminNavBar/AdminNavBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const AdminNavBar = () => {
+  return (
+    <footer className="footer">
+     
+      <p>
+        © 2024, Service360 Management
+      </p>
+    </footer>
+  );
+};
+
+export default AdminNavBar;
